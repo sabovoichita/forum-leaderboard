2cd11fb6a6a277dd1b6467428bf391ba23d2b447
Voichita Maria <sabo_voichita@yahoo.com>
2024-12-10 11:08:13 +0100

    3. Next, access the #posts-container element by using the getElementById() method. Assign it to a new constant called postsContainer.

--- index.js
+++ index.js
@@ -3,3 +3,4 @@ const forumLatest =
 const forumTopicUrl = "https://forum.freecodecamp.org/t/";
 const forumCategoryUrl = "https://forum.freecodecamp.org/c/";
 const avatarUrl = "https://sea1.discourse-cdn.com/freecodecamp";
+const postsContainer = document.getElementById("posts-container");
