8a02ba1cee27264f1f342d67f10cf05d8e766bba
Voichita Maria <sabo_voichita@yahoo.com>
2024-12-10 16:03:59 +0100

    50. Lastly, you will need to return the image for the user avatar.
    Start by adding a return followed by a set of template literals. Inside the template literals, add an img element.
    
    Inside the img tag, add a src attribute with the value of ${userAvatarUrl}. For the alt attribute, add a value of ${user.name}.

--- index.js
+++ index.js
@@ -75,6 +75,7 @@ const avatars = (posters, users) => {
       const userAvatarUrl = avatar.startsWith("/user_avatar/")
         ? avatarUrl.concat(avatar)
         : avatar;
+      return `<img src="${userAvatarUrl}" alt="${user.name}"  value ="${user.name}"/>`;
     }
   });
 };
