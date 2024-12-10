cda9789076198f421bc0da37618e760bc3872259
Voichita Maria <sabo_voichita@yahoo.com>
2024-12-10 14:32:42 +0100

    23. For your timeAgo function, you will want to calculate the difference between the current time and the time of the last activity on a topic. This will allow you to display how much time has passed since a topic had any activity.
    Complete the timeAgo function that meets the following requirements:
    
    If the amount of minutes that have passed is less than 60, return the string "xm ago". x will represent the minutes.
    If the amount of hours that have passed is less than 24, return the string "xh ago". x will represent the hours.
    Otherwise, return the string "xd ago". x will represent the days.
    Here are some equations that will help you calculate the time difference:
    
    minutes = Math.floor((currentTime - lastPost) / 60000);
    hours = Math.floor((currentTime - lastPost) / 3600000);
    days = Math.floor((currentTime - lastPost) / 86400000);

--- index.js
+++ index.js
@@ -9,6 +9,18 @@ const postsContainer = document.getElementById("posts-container");
 const timeAgo = (time) => {
   const currentTime = new Date();
   const lastPost = newDate(time);
+
+  let minutes = Math.floor((currentTime - lastPost) / 60000);
+  let hours = Math.floor((currentTime - lastPost) / 3600000);
+  let days = Math.floor((currentTime - lastPost) / 86400000);
+
+  if (minutes < 60) {
+    return `${minutes}m ago`;
+  } else if (hours < 24) {
+    return `${hours}h ago`;
+  } else {
+    return `${days}d ago`;
+  }
 };
 
 const fetchData = async () => {
