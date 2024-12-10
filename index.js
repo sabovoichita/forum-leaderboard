827f75a6618133fc5975f769066b720f01ea87b2
Voichita Maria <sabo_voichita@yahoo.com>
2024-12-10 15:50:51 +0100

    46. The next step is to find the correct user in the users array.
    Start by creating a constant called user and assign it users.find(). The find method should have a callback function with a parameter of user.
    
    Inside the callback function of the find method, implicitly return the result of checking if user.id is strictly equal to poster.user_id.

--- index.js
+++ index.js
@@ -68,7 +68,9 @@ const viewCount = (views) => {
 };
 
 const avatars = (posters, users) => {
-  return posters.map((poster) => {});
+  return posters.map((poster) => {
+    const user = users.find((user) => user.id === poster.user_id);
+  });
 };
 
 const fetchData = async () => {
