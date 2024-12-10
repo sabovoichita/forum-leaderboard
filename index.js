89f2ee298698af58ec15c25da0dc4e2831d2e624
Voichita Maria <sabo_voichita@yahoo.com>
2024-12-10 11:34:34 +0100

    18. Inside your tr element, add five empty td elements.

--- index.js
+++ index.js
@@ -33,7 +33,13 @@ const showLatestPosts = (data) => {
         category_id,
         bumped_at,
       } = item;
-      return `<tr></tr>`;
+      return `<tr>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+      </tr>`;
     })
     .join("");
 };
