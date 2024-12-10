d50e167f50529175ba1ea183a4626670839aeaa8
Voichita Maria <sabo_voichita@yahoo.com>
2024-12-10 11:09:20 +0100

    4. To populate the forum leaderboard with data, you will need to request the data from an API. This is known as an asynchronous operation, which means that tasks execute independently of the main program flow.
    You can use the async keyword to create an asynchronous function, which returns a promise.
    
    Example Code
    const example = async () => {
      console.log("this is an example");
    };
    Use the async keyword to create an asynchronous arrow function called fetchData.

--- index.js
+++ index.js
@@ -4,3 +4,7 @@ const forumTopicUrl = "https://forum.freecodecamp.org/t/";
 const forumCategoryUrl = "https://forum.freecodecamp.org/c/";
 const avatarUrl = "https://sea1.discourse-cdn.com/freecodecamp";
 const postsContainer = document.getElementById("posts-container");
+
+const fetchData = async () => {
+  console.log("this is an example");
+};
