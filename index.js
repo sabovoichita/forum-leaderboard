e0541005975366a3876bfaccaa797175f46b553b
Voichita Maria <sabo_voichita@yahoo.com>
2024-12-10 15:21:32 +0100

    27. Each of the forum topics includes a category like Python or JavaScript. In the next few steps, you will build out a category object which holds all of the forum categories and classNames for the styling.
    Start by creating a new constant called allCategories and assign it the value of an empty object.

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const avatarUrl = "https://sea1.discourse-cdn.com/freecodecamp";
 
 const postsContainer = document.getElementById("posts-container");
 
+const allCategories = {};
+
 const timeAgo = (time) => {
   const currentTime = new Date();
   const lastPost = new Date(time);
